5f3fe4ceb7fa015c38ec7311bd20c12e29b703c9
Kim Minh <100175752+kim-minh@users.noreply.github.com>
2023-11-23 20:37:35 +0700

    Implement initial money recognition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-video:1.3.0")
+
+    implementation("androidx.camera:camera-view:1.3.0")
+    implementation("androidx.camera:camera-extensions:1.3.0")
+
+    implementation("com.google.mlkit:image-labeling-custom:17.0.2")
 }
--- app/src/main/java/com/kimminh/moneysense/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kimminh/moneysense/ui/home/HomeFragment.kt
@@ -1,14 +1,56 @@
 package com.kimminh.moneysense.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.OptIn
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.mlkit.common.model.LocalModel
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.custom.CustomImageLabelerOptions
 import com.kimminh.moneysense.databinding.FragmentHomeBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
+typealias LabelListener = (label: String) -> Unit
 class HomeFragment : Fragment() {
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && !it.value)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(requireContext(),
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                startCamera()
+            }
+        }
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -27,15 +69,122 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions()
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
         return root
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, MoneyAnalyzer { label ->
+                        binding.recognizedMoney.text = label
+                        Log.d(TAG, "Money: $label")
+                    })
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageAnalyzer)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val TAG = "Money Classification"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        cameraExecutor.shutdown()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+}
+
+private class MoneyAnalyzer(private val listener: LabelListener) : ImageAnalysis.Analyzer {
+    val localModel = LocalModel.Builder()
+        .setAssetFilePath("model.tflite")
+        .build()
+    val customImageLabelerOptions = CustomImageLabelerOptions.Builder(localModel)
+        .setConfidenceThreshold(0.5f)
+        .setMaxResultCount(5)
+        .build()
+    val labeler = ImageLabeling.getClient(customImageLabelerOptions)
+
+    @OptIn(ExperimentalGetImage::class) override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            // Pass image to an ML Kit Vision API
+            labeler.process(image)
+                .addOnSuccessListener { labels ->
+                    for (label in labels) {
+                        val text = label.text
+                        val confidence = label.confidence
+                        val index = label.index
+                        listener(text.substringAfter(' '))
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Classification Error", e.toString())
+                }
+                .addOnCompleteListener {
+                    imageProxy.close()
+                }
+        }
     }
 }
